a5aae3193e96f9c5c303762a82d07a6fa5f780a7
Riket Pokharel <pokharelriket777@gmail.com>
2023-07-30 14:29:23 +0545

    optional chaining

--- JS review/script.js
+++ JS review/script.js
@@ -145,7 +145,7 @@ function getBook(id) {
 
 //Destructuring
 
-const book = getBook(2);
+const book = getBook(3);
 book;
 
 // const title = book.title;
@@ -215,10 +215,19 @@ console.log(book.translations.spanish);
 const spanishTranslation = book.translations.spanish || "NOT TRANSLATED";
 spanishTranslation;
 
-console.log(book.reviews.librarything.reviewsCount);
-const countWrong = book.reviews.librarything.reviewsCount || "no data";
-countWrong;
+// console.log(book.reviews.librarything.reviewsCount);
+// const countWrong = book.reviews.librarything.reviewsCount || "no data";
+// countWrong;
 
-//Nullish coalescing operator (??)
-const count = book.reviews.librarything.reviewsCount ?? "no data";
-count;
+// //Nullish coalescing operator (??)
+// const count = book.reviews.librarything.reviewsCount ?? "no data";
+// count;
+
+//Optional chaining Operator
+function getTotalReivewCount(book) {
+  const goodreads = book.reviews?.goodreads.reviewsCount;
+  const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
+  librarything;
+  return goodreads + librarything;
+}
+console.log(getTotalReivewCount(book));
