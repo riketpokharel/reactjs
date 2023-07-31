3150b6569a51e81412302f00acf1fa9bc1f72309
Riket Pokharel <pokharelriket777@gmail.com>
2023-07-31 19:58:09 +0545

    immutable array

--- JS review/script.js
+++ JS review/script.js
@@ -280,3 +280,23 @@ arr;
 
 const sortedByPages = books.slice().sort((a, b) => b.pages - a.pages);
 sortedByPages;
+
+//IMMUTABLE ARRAYS
+//1) Add book object to array
+const newBook = {
+  id: 6,
+  title: "Harry Potter and the Chamber of Secrets",
+  author: "J.K.Rowling",
+};
+const booksAfterAdd = [...books, newBook];
+booksAfterAdd;
+
+//2) Delete book object from array
+const booksAfterDelete = booksAfterAdd.filter((book) => book.id !== 3);
+booksAfterAdd;
+
+//3) Update book object in the array
+const booksAfterUpdate = booksAfterDelete.map((book) =>
+  book.id === 1 ? { ...book, pages: 1210 } : book
+);
+booksAfterAdd;
