25d8a3769bc6cd22ac53765ecb9c3d9e3b5ef243
Riket Pokharel <pokharelriket777@gmail.com>
2023-08-25 18:00:55 +0545

    travel-list

---
+++ travel-list/src/App.js
@@ -0,0 +1,34 @@
+export default function App() {
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴Far Away 👜</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>👜You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
