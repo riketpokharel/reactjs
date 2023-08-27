9a96719df10858ba9c1a56041d99ac378da73a7b
Riket Pokharel <pokharelriket777@gmail.com>
2023-08-27 13:46:53 +0545

    clearing the list

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Logo from "./Logo";
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -19,6 +20,13 @@ export default function App() {
     );
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirmed) setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
@@ -27,14 +35,11 @@ export default function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
       />
       <Stats items={items} />
     </div>
   );
-}
-
-function Logo() {
-  return <h1>🌴 Far Away 👜</h1>;
 }
 
 function Form({ onAddItems }) {
@@ -79,7 +84,7 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
   const [sortBy, setSortBy] = useState("input");
 
   let sortedItems;
@@ -112,6 +117,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
+        <button onClick={onClearList}>Clear list</button>
       </div>
     </div>
   );
